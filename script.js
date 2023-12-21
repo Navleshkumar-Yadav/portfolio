ca16a96aec718fdc6089087d01d9cf58b8e17d94
Navlesh kumar <119070031+NavleshkumarYadav@users.noreply.github.com>
2023-12-21 15:01:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const typed = new Typed('.multiple-text',{
+    strings: ['FULL STACK DEVELOPER.','CLOUD ARCHITECTURE.','SOFTWARE DEVELOPER.'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDely: 1000,
+    loop:true
+});
